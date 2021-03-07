a8bcc5a4a8d2cf441225ef1c4375934ee9b8c212
Harish Rajagopal <harish.rajagopals@gmail.com>
2021-03-07 15:38:53 +0100

    Added transformations for the data

--- src/data.py
+++ src/data.py
@@ -1,29 +1,32 @@
 """Data loading utilities."""
 from pathlib import Path
-from typing import Any, Optional, Tuple
+from typing import Any, Tuple
 
-from torch import Tensor
+import torch
 from torch.utils.data import Dataset
 from torchvision.io import read_image
+from torchvision.transforms import (
+    Compose,
+    Lambda,
+    RandomCrop,
+    RandomHorizontalFlip,
+    RandomRotation,
+    RandomVerticalFlip,
+)
+from typing_extensions import Final
+
+CROP_SIZE: Final = 128
+ROTATION_RANGE: Final = 10
 
 
 class TrainDataset(Dataset):
     """Dataset for the training data."""
 
-    def __init__(
-        self,
-        root_dir: Path,
-        input_transform: Optional[Any] = None,
-        output_transform: Optional[Any] = None,
-    ):
+    def __init__(self, root_dir: Path):
         """Load the list of images in the dataset.
 
         Args:
             root_dir: Path to the directory where the CIL data is extracted
-            input_transform: The transformation to be applied to the input
-                images
-            output_transform: The transformation to be applied to the output
-                images
         """
         train_dir = root_dir / "training/training"
         self.image_dir = train_dir / "images"
@@ -33,48 +36,70 @@ class TrainDataset(Dataset):
         # Sort for reproducibility
         self.file_names.sort()
 
-        self.input_transform = input_transform
-        self.output_transform = output_transform
+        self.transform = self.get_transform()
 
     def __len__(self) -> int:
         """Return the no. of images in the dataset."""
         return len(self.file_names)
 
-    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor]:
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
         """Get the item at the given index."""
         file_name = self.file_names[idx]
         image = read_image(str(self.image_dir / file_name))
-        if self.input_transform:
-            image = self.input_transform(image)
         ground_truth = read_image(str(self.ground_truth_dir / file_name))
-        if self.output_transform:
-            ground_truth = self.output_transform(ground_truth)
-        return image, ground_truth
+        return self.transform((image, ground_truth))
+
+    @staticmethod
+    def get_transform() -> Any:
+        """Get the transformation for the training data.
+
+        This transform works on both the input and output simultaneously.
+        """
+        transforms = [
+            # Combine them so that random transforms do the same rotation,
+            # crop, etc. for both input and output
+            Lambda(torch.cat),
+            # Scale from uint8 [0, 255] to float32 [0, 1]
+            Lambda(lambda x: x.float() / 255),
+            # Random transformations for data augmentation
+            RandomCrop(CROP_SIZE),
+            RandomRotation(ROTATION_RANGE),
+            RandomHorizontalFlip(),
+            RandomVerticalFlip(),
+            # Split combined tensor into input and output
+            Lambda(lambda x: (x[:3], x[3].unsqueeze(0))),
+            # Threshold the output image
+            Lambda(lambda tup: (tup[0], (tup[1] > 0.5).float())),
+        ]
+        return Compose(transforms)
 
 
 class TestDataset(Dataset):
     """Dataset for the test data."""
 
-    def __init__(self, root_dir: Path, transform: Optional[Any] = None):
+    def __init__(self, root_dir: Path):
         """Load the list of images in the dataset.
 
         Args:
             root_dir: Path to the directory where the CIL data is extracted
-            transform: The transformation to be applied to the images
         """
         image_dir = root_dir / "test_images/test_images"
         # Sort for reproducibility
         self.image_paths = sorted(image_dir.glob("*"))
 
-        self.transform = transform
+        self.transform = self.get_transform()
 
     def __len__(self) -> int:
         """Return the no. of images in the dataset."""
         return len(self.image_paths)
 
-    def __getitem__(self, idx: int) -> Tuple[Tensor, Tensor]:
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
         """Get the item at the given index."""
         image = read_image(str(self.image_paths[idx]))
-        if self.transform:
-            image = self.transform(image)
-        return image
+        return self.transform(image)
+
+    @staticmethod
+    def get_transform() -> Any:
+        """Get the transformation for the test data."""
+        # Scale from uint8 [0, 255] to float32 [0, 1]
+        return Lambda(lambda x: x.float() / 255)
